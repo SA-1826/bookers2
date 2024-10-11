239a9850eed3dea9df4e0fca993a1251bdabe518
EC2 Default User <177551857+SA-1826@users.noreply.github.com>
2024-10-11 15:36:04 +0000

    [Update] 4-27 BooksIndex作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @user = User.find(current_user.id)
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book_params.id)
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end

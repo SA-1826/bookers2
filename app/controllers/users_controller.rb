49369c45e41d2c124ad365b5c79b0836f889969a
EC2 Default User <177551857+SA-1826@users.noreply.github.com>
2024-10-16 15:33:54 +0000

    [Update] 4-27 43行目まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
